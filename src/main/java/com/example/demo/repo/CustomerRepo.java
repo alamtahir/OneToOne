b477db1bd7f8edb6a2a91bf678976ad21d937246
alamtahir <tahiralamtahiralamtahir@gmail.com>
2025-12-01 16:31:24 +0530

    wrongly pushed/dev4

--- src/main/java/com/example/demo/repo/CustomerRepo.java
+++ src/main/java/com/example/demo/repo/CustomerRepo.java
@@ -2,9 +2,10 @@ package com.example.demo.repo;
 
 import com.example.demo.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface CustomerRepo extends JpaRepository<Customer, UUID> {
 
 }
