2afa5c5c81d5295a2889d85ee85b8cd38cf18a76
alamtahir <tahiralamtahiralamtahir@gmail.com>
2025-12-01 15:51:53 +0530

    change pom.xml file

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -1,20 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.entity.Customer;
+import com.example.demo.entity.Orders;
+import com.example.demo.repo.CustomerRepo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.example.demo.entity.Profile;
-import com.example.demo.entity.User;
-import com.example.demo.repo.UserRepo;
-
 @SpringBootApplication
 public class Demo2Application {
 
-	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(Demo2Application.class, args);
-
-
+	public Demo2Application() {
 	}
 
+	public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(Demo2Application.class, args);
+		Customer customer = new Customer();
+		customer.setName("John Doe");
+		Orders order1 = new Orders();
+		order1.setProductName("Laptop");
+		Orders order2 = new Orders();
+		order2.setProductName("Smartphone");
+		order1.setCustomer(customer);
+		order2.setCustomer(customer);
+		customer.getOrders().add(order1);
+		customer.getOrders().add(order2);
+		CustomerRepo repo = ctx.getBean(CustomerRepo.class);
+		repo.save(customer);
+
+	}
 }
