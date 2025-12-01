c83ba67837bb3709237a4bf1831fa008efa73134
alamtahir <tahiralamtahiralamtahir@gmail.com>
2025-12-01 16:22:39 +0530

    refactor code/dev4

--- src/main/java/com/example/demo/Demo2Application.java
+++ src/main/java/com/example/demo/Demo2Application.java
@@ -1,17 +1,11 @@
 package com.example.demo;
-import com.example.demo.entity.FoodItem;
-import com.example.demo.entity.FoodOrder;
-import com.example.demo.repo.FoodRepo;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-
-import java.util.List;
-
 @SpringBootApplication
 public class Demo2Application {
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = SpringApplication.run(Demo2Application.class, args);
+	SpringApplication.run(Demo2Application.class, args);
 
 	}
 }
